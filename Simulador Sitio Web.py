16c75b221fa5d35535a966e9f7b778b8170b4184
dmunofer <diego.fmz@hotmail.com>
2022-04-05 02:54:46 +0200

    excepción actualizada

--- Simulador Sitio Web.py
+++ Simulador Sitio Web.py
@@ -10,6 +10,6 @@ elif correo =='':
     raise Exception('Es un una entrada incorrecta. Introduzca una dirección válida')
 
 elif len(correo)==5:
-    raise Exception('')
+    raise Exception('Cuenta bloqueada a causa de un ataque')
 else:
     raise Exception('Una dirección de correo debe tener el formato xxx@xxx.xx')
