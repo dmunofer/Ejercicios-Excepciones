2f515c9ef6c6881b13c20d240b36a08488c600e9
dmunofer <diego.fmz@hotmail.com>
2022-04-05 03:05:55 +0200

    no funciona

--- Simulador Sitio Web.py
+++ Simulador Sitio Web.py
@@ -3,14 +3,14 @@ import re
 
 correo=input('Introduzca su correo electrónico')
 
-if re.search(". * @. * \ .. *", correo):
+if re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$' , correo):
     cadenas = correo.split('@')
     cadena_final = '¡Bienvenido '+ cadenas[0] +'!'
     print(cadena_final)
 
 
 elif correo =='':
-    raise Exception('Es un una entrada incorrecta. Introduzca una dirección válida')
+    raise Exception('Es un una entrada incorrecta. Introduzca una dirección de correo')
 
 elif len(correo)==5:
     raise Exception('Cuenta bloqueada a causa de un ataque')
